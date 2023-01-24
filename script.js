8f341e6dd9e009f50e34aaca76b4e09f2056a4f3
Malika <sirojidddinovamalika06@gmail.com>
2023-01-24 20:00:32 +0500

    full project

---
+++ script.js
@@ -0,0 +1,29 @@
+var str = prompt('Введите свое имя')
+var str = +prompt('А теперь ваш возраст')
+alert('Решите следующие примеры')
+var str1 = +prompt('4+3=')
+var str2 = +prompt('8-5=')
+var str3 = +prompt('3*5=')
+var str4 = +prompt('36/9=')
+var str5 = +prompt('26%4=')
+alert('Чтобы узнать свой результат перейдите в консоль')
+
+var answer1 = 7;
+var answer2 = 3;
+var answer3 = 15;
+var answer4 = 4;
+var answer5 = 2;
+
+var question1 = '4+3=';
+var question2 = '8-5=';
+var question3 = '3*5=';
+var question4 = '36/9=';
+var question5 = '26%4=';
+
+console.log( question1 + answer1 + ' (Ваш ответ ' + str1 + ')' );
+console.log( question2 + answer2 + ' (Ваш ответ ' + str2 + ')' );
+console.log( question3 + answer3 + ' (Ваш ответ ' + str3 + ')' );
+console.log( question4 + answer4 + ' (Ваш ответ ' + str4 + ')' );
+console.log( question5 + answer5 + ' (Ваш ответ ' + str5 + ')' );
+
+
